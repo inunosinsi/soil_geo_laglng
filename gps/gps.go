e552c588a02ea6d1a02bc403186ab1a92b227e30
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2018-08-16 14:19:34 +0900

    GPS用のパッケージ

---
+++ gps/gps.go
@@ -0,0 +1,8 @@
+package gps
+
+func LogLatLng(lat float32, lng float32) bool {
+
+	/** @ToDo 緯度経度を登録。登録時に現在の時刻のタイムスタンプも一緒に記録する **/
+
+	return false
+}
---
+++ gps/gps_test.go
@@ -0,0 +1,7 @@
+package gps
+
+import "testing"
+
+func TestLogLatLng(t *testing.T) {
+
+}
